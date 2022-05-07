5208ff5dd758c224c6c1aa7ea5d764f86bb31acc
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2022-05-07 19:18:42 +0530

    Time: 14 ms (40.26%), Space: 12.5 MB (71.89%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -6,8 +6,8 @@ public:
         vector<int> ans(2*n);
         for(int i=0;i<n;i++)
         {
-          ans[i]=nums[i];
-          ans[i+n]=nums[i];
+          ans[i]=ans[i+n]=nums[i];
+          
         }
         return ans;
     }
