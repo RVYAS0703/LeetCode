29ada139963b64641f9cefabe151a0472c5d9dd0
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2022-07-13 17:42:22 +0530

    Time: 19 ms (66.07%), Space: 10.9 MB (31.98%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        // int p1=nums[0];
-        // int p2=nums[p1+1];
-        // sort(nums.begin(),nums.end());
-        vector<int> v1;
-        for(int i=0;i<nums.size();i++)
-        {
-            int p2=target-nums[i];
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(p2==nums[j])
-                {
-                    v1.push_back(i);
-                    v1.push_back(j);
-                }
+  vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> freqMap;
+        vector<int> ans(2,-1);
+        for(int i=0;i<nums.size();i++){
+            int element = target - nums[i];
+            if(freqMap.count(element) > 0){
+                ans[0] = freqMap[element];
+                ans[1] = i;
+                return ans;
             }
+            
+            freqMap[nums[i]] = i;
         }
-        return v1;
-    }
-};
+        
+        return ans;
+  } 
+    };
