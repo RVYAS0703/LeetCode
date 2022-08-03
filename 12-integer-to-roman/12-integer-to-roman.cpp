a7f7e503152e91a4191a13cd4a94f8f00f1c2b66
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2022-08-04 00:52:56 +0530

    Time: 12 ms (57.06%), Space: 5.9 MB (91.63%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,74 +1,18 @@
 class Solution {
 public:
+    const int rom_val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    const string rom_str[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     string intToRoman(int num) {
+        string out_string = "";
         
-        string ans="";
-        while(num)
-        {
-            if(num>=1000)
-            {
-                ans+='M';
-                num-=1000;
-            }
-            else if(num>=900){
-                ans+="CM";
-                num-=900;
-                
-            }
-            else if(num>=500){
-                ans+="D";
-                num-=500;
-                
-            }
-            else if(num>=400){
-                ans+="CD";
-                num-=400;
-                
-            }
-            else if(num>=100){
-                ans+="C";
-                num-=100;
-                
-            }
-            else if(num>=90){
-                ans+="XC";
-                num-=90;
-                
-            }else if(num>=50){
-                ans+="L";
-                num-=50;
-                
-            }
-            else if(num>=40){
-                ans+="XL";
-                num-=40;
-                
-            }
-            else if(num>=10){
-                ans+="X";
-                num-=10;
-                
-            }
-            else if(num>=9){
-                ans+="IX";
-                num-=9;
-                
-            }
-            else if(num>=5){
-                ans+="V";
-                num-=5;
-                
-            }
-            else if(num>=4){
-                ans+="IV";
-                num-=4;
-                
-            }
-            else if(num>=1){
-                ans+='I';
-                num--;
+		//Iterate through the lookup table subtracting the largest possible value from the input number.
+        for (int i = 0; i < 13; i++) {
+            while (num - rom_val[i] >= 0) {
+                out_string += rom_str[i];
+                num -= rom_val[i];
             }
         }
-        return ans;
+        
+        return out_string;
     }
 };
