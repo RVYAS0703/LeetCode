6cd1e0a70d269f0a08b72ed2bc748e1f1c075b01
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2023-03-27 04:25:05 -0700

    Time: 3 ms (99.88%), Space: 15.8 MB (97.13%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
-  int k=v.size()/3;
-  unordered_map<int, int> mp;
-  for (int i:v){
-    mp[i]++;
-  }
-  v.clear();
-  for(auto it = mp.begin(); it != mp.end(); it++){
-    if(it->second > k){
-      v.push_back(it->first);
-    }
-  
-  }
-        
-        return v;
+    vector<int> majorityElement(vector<int>& nums) {
+         int num1 = INT_MIN, num2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            else if(num2 == element){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = element;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = element;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> output;
+        int countMajority = nums.size()/3;
+        count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            if(num2 == element){
+                count2++;
+            }
+        }
+        if(count1 > countMajority){
+            output.push_back(num1);
+        }
+        if(count2 > countMajority){
+            output.push_back(num2);
+        }
+        return output;
     }
 };
