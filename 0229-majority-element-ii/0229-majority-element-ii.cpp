7d3c87de05acb9dd4f597f96575da7c7452fd82c
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2023-03-27 04:29:22 -0700

    Time: 18 ms (33.41%), Space: 16 MB (21.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,45 +1,46 @@
 class Solution {
+
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-         int num1 = INT_MIN, num2 = INT_MIN;
-        int count1 = 0, count2 = 0;
-        for(auto element : nums){
-            if(num1 == element){
-                count1++;
-            }
-            else if(num2 == element){
-                count2++;
-            }
-            else if(count1 == 0){
-                num1 = element;
-                count1 = 1;
-            }
-            else if(count2 == 0){
-                num2 = element;
-                count2 = 1;
-            }
-            else{
-                count1--;
-                count2--;
-            }
-        }
-        vector<int> output;
-        int countMajority = nums.size()/3;
-        count1 = 0, count2 = 0;
-        for(auto element : nums){
-            if(num1 == element){
-                count1++;
-            }
-            if(num2 == element){
-                count2++;
-            }
-        }
-        if(count1 > countMajority){
-            output.push_back(num1);
-        }
-        if(count2 > countMajority){
-            output.push_back(num2);
-        }
-        return output;
+    vector<int> majorityElement(vector<int>& v) {
+        int num1=INT_MIN,num2=INT_MIN,cnt1=0,cnt2=0;
+
+  for(int el:v){
+    if(el==num1) cnt1++;
+
+    else if (el==num2) cnt2++;
+
+    else if(cnt1==0){
+      num1=el;
+      cnt1++;
+    }
+    else if(cnt2==0){
+      num2=el;
+      cnt2++;
+    }
+
+    else{
+      cnt1--;
+      cnt2--;
+    }
+  }
+
+  vector<int>ans;
+  cnt1=cnt2=0;
+  for(int i=0;i<v.size();i++)
+    {
+      if(v[i]==num1) cnt1++;
+
+      if(v[i]==num2) cnt2++;
+    }
+
+  if(cnt1>v.size()/3){
+    ans.push_back(num1);
+  }
+  if(cnt2>v.size()/3){
+    ans.push_back(num2);
+  }
+        
+        return ans;
     }
 };
+    
