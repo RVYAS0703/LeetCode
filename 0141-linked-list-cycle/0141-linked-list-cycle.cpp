fa347274a429459ed0f817d8e06645b84352e053
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2023-06-30 00:16:53 +0530

    Time: 11 ms (74.49%), Space: 7.9 MB (99.99%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,26 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode * s=head;
-        ListNode * f=head;
-        
-        if(head==NULL)
-        {
-            return false;
-        }
-        
+        ListNode* s=head;
+        ListNode* f=head;
         while(f!=NULL && f->next!=NULL)
         {
             s=s->next;
             f=f->next->next;
             
             if(s==f)
-        {
-            return true;
+            {
+                return true;
+            }
         }
-        }
-        
-        
         return false;
     }
 };
