df8350d9b94d8ed90e3173d8d7b6b2bd74383a57
RVYAS0703 <85107706+RVYAS0703@users.noreply.github.com>
2023-07-09 09:04:46 +0530

    Time: 59 ms (49.55%), Space: 16.3 MB (77.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,24 +1,24 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        n = len(nums)
-    
-        # Find the first index from the right where nums[i] < nums[i+1]
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-
-        if i >= 0:
-            # Find the first index from the right where nums[j] > nums[i]
-            j = n - 1
-            while j >= 0 and nums[j] <= nums[i]:
-                j -= 1
-            # Swap nums[i] and nums[j]
-            nums[i], nums[j] = nums[j], nums[i]
-
-        # Reverse the suffix starting from index i+1
-        left = i + 1
-        right = n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        i=n-2 # its the last where i can have a dip. [1,2,3,4,5] observe it 4 can only chnage with 5  
+        while i>=0 and nums[i]>=nums[i+1]:
+            i-=1
+        
+        if i>=0:
+            j=n-1
+            while j>=0 and nums[i]>=nums[j]:
+                j-=1
+                
+            nums[i],nums[j]=nums[j],nums[i]
+            
+        left=i+1
+        right=n-1
+        while left<right:
+            nums[left],nums[right]=nums[right],nums[left]
+            left+=1
+            right-=1
+            
